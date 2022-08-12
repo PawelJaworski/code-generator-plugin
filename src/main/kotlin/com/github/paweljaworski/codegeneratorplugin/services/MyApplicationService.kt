dc6575837739c3d2922776550fc91cfbdb989e84
GitHub Action <action@github.com>
2022-08-12 06:45:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/paweljaworski/codegeneratorplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/paweljaworski/codegeneratorplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.paweljaworski.codegeneratorplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.paweljaworski.codegeneratorplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/paweljaworski/codegeneratorplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/paweljaworski/codegeneratorplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.paweljaworski.codegeneratorplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.paweljaworski.codegeneratorplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
