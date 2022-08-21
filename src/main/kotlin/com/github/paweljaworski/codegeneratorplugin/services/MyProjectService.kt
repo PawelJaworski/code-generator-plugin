01bc0f8f67ef4df5b00b69494def2da85703f94d
paweljaworski <pawel.jaworski@intive.com>
2022-08-21 22:19:42 +0200

    java file generation

--- src/main/kotlin/com/github/paweljaworski/codegeneratorplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/paweljaworski/codegeneratorplugin/services/MyProjectService.kt
@@ -2,13 +2,12 @@ package com.github.paweljaworski.codegeneratorplugin.services
 
 import com.intellij.openapi.project.Project
 import com.github.paweljaworski.codegeneratorplugin.MyBundle
+import com.intellij.openapi.vfs.VirtualFile
 
 class MyProjectService(project: Project) {
 
     init {
         println(MyBundle.message("projectService", project.name))
 
-        System.getenv("CI")
-            ?: TODO("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
     }
 }
